881ce89f5202557a7bb22ef203fa9448f42215f0
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-23 15:16:17 +0530

    Update link.py

--- link.py
+++ link.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 # Your session string
 SESSION ="BQFmONcASNSlWTTCENlUwDdYnMbA4Vplog6_plkvjhA2v6I1oN_Z2rJcbPMQtSYMR90BlvynlSa3SSQz_sCxPYkm39ffXdO_k-TKpx-KlHMKN-qhL1wV1NgPJaephiAl4kBB1S9JRZP8fSiC5OUdhnpCMVqmY-ma5XBaSsXRLR41RMKlmp2VK4aJvg2fXCUJ4FHph6b96eqPdcLtjc0KqqU1altrPgfwwDh-_hPkx1rudtTle4BEJbJAtdXTsY7HMNqPQI7vxF88bejzYs9F0XdU_bjDIVhaghnw8g_J1TVUmvzMAjvjBmIuzsrXv_rNCMoTNLVb-P080qbI36LzHjy6DQ4I-gAAAAFQUsvEAA"
 
-APP_ID="23476439" 
+APP_ID=23476439
 API_HASH="1626e884119a29dbccbb78e39b48128f"
 
 # Save the session string to a file
