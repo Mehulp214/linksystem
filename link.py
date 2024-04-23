14d73c504b939f88b99a03f944a2b78e9554fe54
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-23 15:12:17 +0530

    Update link.py

--- link.py
+++ link.py
@@ -1,10 +1,16 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-
+# Your session string
 SESSION ="BQFmONcASNSlWTTCENlUwDdYnMbA4Vplog6_plkvjhA2v6I1oN_Z2rJcbPMQtSYMR90BlvynlSa3SSQz_sCxPYkm39ffXdO_k-TKpx-KlHMKN-qhL1wV1NgPJaephiAl4kBB1S9JRZP8fSiC5OUdhnpCMVqmY-ma5XBaSsXRLR41RMKlmp2VK4aJvg2fXCUJ4FHph6b96eqPdcLtjc0KqqU1altrPgfwwDh-_hPkx1rudtTle4BEJbJAtdXTsY7HMNqPQI7vxF88bejzYs9F0XdU_bjDIVhaghnw8g_J1TVUmvzMAjvjBmIuzsrXv_rNCMoTNLVb-P080qbI36LzHjy6DQ4I-gAAAAFQUsvEAA"
-# Initialize Pyrogram client
-app = Client(SESSION)
+
+# Save the session string to a file
+with open("session.txt", "w") as file:
+    file.write(SESSION)
+
+# Initialize Pyrogram client with the session file
+app = Client("my_bot", config_file="session.txt")
+
 # Function to handle messages
 @app.on_message(filters.me & filters.reply & filters.command("link", prefixes="."))
 async def handle_link_command(client, message: Message):
