4bf08da98ea2a28871259b13e03605d49d9c4ac9
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-23 15:32:18 +0530

    Update link.py

--- link.py
+++ link.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
 
 # Your session string
 SESSION = "BQFmONcASNSlWTTCENlUwDdYnMbA4Vplog6_plkvjhA2v6I1oN_Z2rJcbPMQtSYMR90BlvynlSa3SSQz_sCxPYkm39ffXdO_k-TKpx-KlHMKN-qhL1wV1NgPJaephiAl4kBB1S9JRZP8fSiC5OUdhnpCMVqmY-ma5XBaSsXRLR41RMKlmp2VK4aJvg2fXCUJ4FHph6b96eqPdcLtjc0KqqU1altrPgfwwDh-_hPkx1rudtTle4BEJbJAtdXTsY7HMNqPQI7vxF88bejzYs9F0XdU_bjDIVhaghnw8g_J1TVUmvzMAjvjBmIuzsrXv_rNCMoTNLVb-P080qbI36LzHjy6DQ4I-gAAAAFQUsvEAA"
@@ -16,13 +15,11 @@ app = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id
 
 # Function to handle messages
 @app.on_message(filters.me & filters.reply & filters.command("link", prefixes="."))
-async def handle_link_command(client, message: Message):
+async def handle_link_command(client, message):
     # Check if the replied message exists and is from a bot
-    if message.reply_to_message and message.reply_to_message.sender_chat:
-        # Get the message ID of the replied message
-        replied_msg_id = message.reply_to_message.message_id
+    if message.reply_to_message and message.reply_to_message.sender_chat and message.reply_to_message.sender_chat.is_bot:
         # Generate a link using the message ID
-        link = f"https://t.me/{message.reply_to_message.sender_chat.username}/{replied_msg_id}"
+        link = f"https://t.me/{message.reply_to_message.sender_chat.username}/{message.reply_to_message.message_id}"
         # Edit the .link message with the generated link
         await message.edit_text(link)
     else:
