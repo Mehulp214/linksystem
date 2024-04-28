5cab423c6f5f55e002060be9751ae7a8c70557e0
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-04-28 19:48:20 +0530

    Update link.py

--- link.py
+++ link.py
@@ -25,7 +25,7 @@ async def handle_link_command(client, message):
     # Check if the replied message exists and is from a bot
     await client.edit_message_text(
             chat_id=message.chat.id,
-            message_id=message.reply_to_message.message_id,
+            message_id=message.reply_to_message.id,
             text=f"https://t.me/b/{chat_id}/{message_id}"
     )
     print(id,chat_id)
