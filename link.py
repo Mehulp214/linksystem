3d9e3baf634c3e4ac0d4bc5402892bb288ebf2ae
Mehulp214 <114227971+Mehulp214@users.noreply.github.com>
2024-05-10 16:10:11 +0530

    Update link.py

--- link.py
+++ link.py
@@ -21,15 +21,18 @@ async def handle_link_command(client, message):
         # Check if the replied message exists and is from a bot
         msg_id = message.reply_to_message.id
         chat_id = message.reply_to_message.from_user.username
-        msg = await client.get_messages(chat_id, msg_id)
-        msg = await message.reply_text("Genrating link")
-        await msg.edit_text(f"https://t.me/b/{chat_id}/{msg_id}")
-        await message.edit_text("hello")
+        await message.edit_text("Generating link...")
+        await message.delete()
+        await message.reply_text(f"https://t.me/b/{chat_id}/{msg_id}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
- 
+ #await client.edit_message_text(
+ #   chat_id=message.chat.id,
+ #   message_id=message.id,
+ #   text="hello"
+#)
 
 # Run the userbot
 app.run()
